d94cd7f4b219dad7e5f75e958ca5ec78970c4400
Morgan <amorgan680@gmail.com>
2024-07-18 23:53:32 -0500

    setup models, updated date formatting

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,4 +1,6 @@
 //date formatting
+const { format } = require('date-fns');
+
 module.exports = (timestamp) => {
-  return new Date(timestamp).toLocaleString();
+  return format(new Date(timestamp), 'yyyy-MM-dd HH:mm:ss');
 };
